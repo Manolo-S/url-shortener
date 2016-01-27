c3c43e4ddc303df4dc6faf3d5bef4cd159d8cf3f
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-01-27 20:08:27 -0200

    add url not found message

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ var express = require('express');
 var app = express();
 var validUrl = require('valid-url');
 var port = process.env.PORT || 3000;
-
 var mongoose = require('mongoose');
-
 var db = mongoose.connect('mongodb://piet:snot1@ds051615.mongolab.com:51615/shorten-your-url');
-
 var Schema = mongoose.Schema;
 
 var shortUrlSchema = new Schema({
@@ -23,18 +20,20 @@ var shortUrlModel= mongoose.model('urlshort', shortUrlSchema);
 function checkURL(req, res){
 	var url = (req.originalUrl).slice(5);
 	var response;
+	var newUrlCount;
+	var herokuAppUrl = 'http://shorten-your-url.herokuapp.com/'
 	console.log('url: ', url)
 	if (validUrl.isUri(url)){
 		shortUrlModel.find({originalUrl: url}, function(err,obj){
 			if (obj[0]){
-				response = JSON.stringify({"original_url": url, short_url: 'http://shorten-your-url.herokuapp.com/' + obj[0].urlNumber});   
+				response = JSON.stringify({"original_url": url, short_url: herokuAppUrl + obj[0].urlNumber});   
 				res.setHeader('Content-Type', 'text/html');
 				res.send('<p>' + response + '</p>');
 			} else {	
 				console.log('url not found')	;
 				shortUrlModel.count({}, function (err, count) {
-					var newUrlCount = count + 1;
-					response = JSON.stringify({"original_url": url, short_url: 'http://shorten-your-url.herokuapp.com/' + newUrlCount});   
+					newUrlCount = count + 1;
+					response = JSON.stringify({"original_url": url, short_url: herokuAppUrl + newUrlCount});   
 					res.setHeader('Content-Type', 'text/html');
 					res.send('<p>' + response + '</p>');
 					storeShortUrl(url, newUrlCount);					
@@ -52,22 +51,23 @@ function checkURL(req, res){
 
 function storeShortUrl(url, newUrlCount){
 	shortUrlModel.create({
-	urlNumber: newUrlCount,
-	originalUrl: url
-}, function(err, urlObj){
-	  if(!err){
-	  	console.log(urlObj);
-	  }
-	});
+						urlNumber: newUrlCount,
+						originalUrl: url
+						}, function(err, urlObj){
+							  if(!err){
+							  	console.log(urlObj);
+							  }
+					       }
+					   );
 }
 
 function shortUrl(req, res){
-
 	var id = req.params.id;
+	var obj;
     
 	shortUrlModel.find({urlNumber: id}, function(err, storedUrl){
 		if (storedUrl[0] !== undefined) {
-			var obj = storedUrl[0];
+			obj = storedUrl[0];
 			console.log(obj.originalUrl)
 			res.redirect(obj.originalUrl);
 		}
